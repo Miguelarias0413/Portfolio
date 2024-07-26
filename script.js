de7a006894015fef7c21add0b0c23edf7e9bde11
Miguel Arias <miguelarias0413@gmail.com>
2024-07-26 03:24:40 -0500

    Implementacion hoverMouse duranto toda la pagina y con apertura a projectsCards hecha totalmente con javascript y mejoras css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function main(){
     observeSkills()
     observeSectionsToHeaderElements()
-
+    mouseHoverHandler();
 
 }
 function observeSectionsToHeaderElements(){
@@ -55,6 +55,67 @@ function observeSkills(){
     skillItems.forEach(item=>{
         observer.observe(item)
     })
+}
+function mouseHoverHandler(){
+    const divHover = document.querySelector('.mousehover__element');
+    const projectsCards = document.querySelectorAll('.projects__card')
+
+
+    document.addEventListener('mousemove',getMouseMovement)
+
+    function getMouseMovement(e){
+        let mouseXPosition = e.clientX;
+        let mouseYPosition = e.clientY;
+        setMousePositionToDivHover(mouseXPosition,mouseYPosition);
+    }
+    function setMousePositionToDivHover(X,Y){
+        divHover.style.left = `${X - divHover.offsetWidth / 2}px`;
+        divHover.style.top = `${Y - divHover.offsetHeight / 2}px`;
+    }
+
+    projectsCardDivHoverController()
+    function projectsCardDivHoverController(){
+        const divHoverContainer = document.querySelector('.mousehover__container');
+        function mouseGettingIntoProjectCard(e){
+            
+            
+            //Establecer Posicion
+            e.target.appendChild(divHover);
+            
+                
+                setTimeout(()=>{
+                    divHover.style.top = e.offsetY - divHover.offsetHeight/2;
+                    divHover.style.left = e.offsetX- divHover.offsetWidth/2;
+
+                },10)
+                
+            document.removeEventListener('mousemove',getMouseMovement)
+            
+            //Establecer ESTILOS
+            setTimeout(()=>{
+                divHover.classList.add('mousehover__element--inside')
+
+            },10)
+            
+        }
+        function mouseGettingOutFromProjectCard(e){
+            divHover.classList.remove('mousehover__element--inside')
+            divHoverContainer.appendChild(divHover);
+
+            document.addEventListener('mousemove',getMouseMovement)
+            console.log(e.target);
+          
+
+            
+        }
+
+
+        projectsCards.forEach(projectCard=>{
+            projectCard.addEventListener('mouseenter',mouseGettingIntoProjectCard);
+            projectCard.addEventListener('mouseleave',mouseGettingOutFromProjectCard);
+        })
+    }
+ 
 }
 
 addEventListener('DOMContentLoaded',main)
